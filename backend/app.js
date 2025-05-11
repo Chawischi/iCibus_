3465502e86e0635f03697de7bfafe3275c3095fa
Gabriel Bandoch <96153388+GabrielBandoch@users.noreply.github.com>
2025-05-10 22:29:41 -0300

    Revertendo para o estado do commit a1b3099

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,30 @@
 const express = require('express');
-const cors = require('cors');
-const path = require('path');
-require('dotenv').config();
-
+const cors = require('cors');  // Para permitir requisições do front-end
+require('dotenv').config(); // Carrega variáveis do .env ou .env.local
 const sequelize = require('./config/db.js');
-const User = require('./models/User.js');
+const User = require('./models/User.js')
 const authRoutes = require('./routes/authRoutes.js');
-const restaurantRoutes = require('./routes/restaurantRoutes.js'); // <-- ADICIONADO
 
 const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'uploads'))); // Serve imagens
+app.use(express.json());  // Para interpretar o corpo das requisições como JSON
 
 // Rotas
 app.use('/auth', authRoutes);
-app.use('/restaurants', restaurantRoutes); // <-- ADICIONADO
 
-// Sincronização com o banco
-sequelize.sync({ force: false })
+
+sequelize.sync({ force: false }) 
   .then(() => {
     console.log('Banco sincronizado');
   })
   .catch(err => console.error('Erro ao sincronizar o banco:', err));
 
 // Inicia o servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
+
